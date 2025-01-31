56b891d873ce408615259788d0af30641ffac44b
Rohit Verma <rhtvma@outlook.com>
2025-01-31 20:46:17 +0530

    RESET button

--- src/components/App.js
+++ src/components/App.js
@@ -12,15 +12,25 @@ const App = () => {
     setCount(count - 1)
     }
 
+    const handleReset = ()=> {
+        setCount(0)
+    }
+
     return (
         <div className="App">
             <div className="box">
-                <p> count - {count}</p>
+                <p> {count}</p>
                 <button onClick={handleAdd} className="add">
                     ADD
                 </button>
+                {/* <button onClick={()=>{setCount(count + 1)}} className="add">
+                    ADD direct calling
+                </button> */}
                 <button onClick={handleSub} className="sub">
                     SUB
+                </button>
+                <button onClick={handleReset} className="reset">
+                    RESET
                 </button>
             </div>
         </div>
