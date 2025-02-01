19798d95729504cfb8c739e8a5818522261b85c1
Rohit Verma <rhtvma@outlook.com>
2025-02-01 14:18:47 +0530

    task component
    task component
    header
    css

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,16 @@
-import { useState } from "react";
-
 import "./App.css"
+import TaskList from "./Tasks/TaskList"
+import Header from "./Header"
+import Footer from "./Footer"
+
+
 const App = () => {
-    const [count, setCount] = useState(0);
-
-    const handleAdd = () => {
-        setCount(count + 1)
-    }
-
-    const handleSub = () => {
-    setCount(count - 1)
-    }
-
-    const handleReset = ()=> {
-        setCount(0)
-    }
-
     return (
         <div className="App">
-            <div className="box">
-                <p> {count}</p>
-                <button onClick={handleAdd} className="add">
-                    ADD
-                </button>
-                {/* <button onClick={()=>{setCount(count + 1)}} className="add">
-                    ADD direct calling
-                </button> */}
-                <button onClick={handleSub} className="sub">
-                    SUB
-                </button>
-                <button onClick={handleReset} className="reset">
-                    RESET
-                </button>
-            </div>
+            <Header />
+            <TaskList />
         </div>
-    );
+    )
 }
 
 export default App
